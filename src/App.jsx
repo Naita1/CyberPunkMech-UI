a0bcbf95f40ce7b7c0af7ab6d56aecdcae508510
Naita1 <tainaribeir1930@gmail.com>
2026-08-02 22:16:38 -0300

    Implement smooth page transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import Home from './pages/Home/Home';
 import Profile from './pages/Profile/Profile';
 import './App.css';
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransitionStage] = useState('fadeEnter');
+
+  useEffect(() => {
+    setTransitionStage('fadeExit');
+
+    const timeout = setTimeout(() => {
+      setDisplayLocation(location);
+      setTransitionStage('fadeEnter');
+    }, 180);
+
+    return () => clearTimeout(timeout);
+  }, [location]);
+
   return (
-    <BrowserRouter>
-      <Routes>
+    <div className={`route-transition ${transitionStage}`}>
+      <Routes location={displayLocation}>
         <Route path="/" element={<Home />} />
         <Route path="/perfil" element={<Profile />} />
       </Routes>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AnimatedRoutes />
     </BrowserRouter>
   );
 }
